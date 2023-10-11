048c47cf92b5aa86d8e8837b7894ede585816946
An Vu <hoanganwm@gmail.com>
2023-10-11 17:08:29 +0700

    add-model-and-dictionary.txt

--- src/com/group/assignment2/model/Dictionary.java
+++ src/com/group/assignment2/model/Dictionary.java
@@ -9,5 +9,27 @@ package com.group.assignment2.model;
  * @author hoang
  */
 public class Dictionary {
-    
+    private String englishWord;
+    private String vietnameseWord;
+
+    public Dictionary(String englishWord, String vietnameseWord) {
+        this.englishWord = englishWord;
+        this.vietnameseWord = vietnameseWord;
+    }
+
+    public String getEnglishWord() {
+        return englishWord;
+    }
+
+    public void setEnglishWord(String englishWord) {
+        this.englishWord = englishWord;
+    }
+
+    public String getVietnameseWord() {
+        return vietnameseWord;
+    }
+
+    public void setVietnameseWord(String vietnameseWord) {
+        this.vietnameseWord = vietnameseWord;
+    }
 }
